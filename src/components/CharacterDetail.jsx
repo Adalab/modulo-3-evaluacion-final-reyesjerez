156ca06494a44456dc1fe06d61bf82a5a1a01ee2
reyesjerez <reyesjerezpe@gmail.com>
2024-02-17 16:36:58 +0100

    Add characters details

--- src/components/CharacterDetail.jsx
+++ src/components/CharacterDetail.jsx
@@ -1,4 +1,7 @@
 import { useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
+
+import imageGryffindor from "../images/houses/gryffindor.gif";
 
 function CharacterDetail({ findCharacter }) {
   const params = useParams();
@@ -6,27 +9,54 @@ function CharacterDetail({ findCharacter }) {
   const character = findCharacter(params.id);
 
   return (
-    <div className="detail">
-      <h3>Información detallada de {character.name}</h3>
-      <img src={character.image} alt="Foto de " />
-      <h4 className="detail__name">Nombre::</h4>
-      <p className="detail__name__text">{character.name}</p>
-      <h5 className="detail__name__title">Nombres alternativos:</h5>
-      <p className="detail__name__text">{character.alternate_names}</p>
-      <h5 className="detail__name__title">Género:</h5>
-      <p className="detail__name__text">{character.gender}</p>
-      <h5 className="detail__name__title">Fecha de nacimiento:</h5>
-      <p className="detail__name__text">{character.dateOfBirth}</p>
-      <p className="detail__name__text"></p>
-      <h5 className="detail__name__title">Vivo:</h5>
-      <p className="detail__name__text">{character.alive}</p>
-      <h5 className="detail__name__title">Especie:</h5>
-      <p className="detail__name__text">{character.species}</p>
-      <h5 className="detail__name__title">Casa:</h5>
-      <p className="detail__name__text">{character.house}</p>
-      <h5 className="detail__name__title">Categoría de mago:</h5>
-      <p className="detail__name__text">{character.wizard}</p>
-    </div>
+    <>
+      <h3 className="detail__name">
+        Información detallada de {character.name}
+      </h3>
+      <div className="detail">
+        <section>
+          <img
+            className="detail__img"
+            src={character.image || imageGryffindor}
+            alt="Foto de "
+          />
+        </section>
+        <section className="detail__container">
+          <h5 className="detail__title">
+            Nombre: <span className="detail__text">{character.name}</span>
+          </h5>
+
+          <h5 className="detail__title">
+            Nombres alternativos:{" "}
+            <span className="detail__text">{character.alternate_names}</span>
+          </h5>
+
+          <h5 className="detail__title">
+            Género: <span className="detail__text">{character.gender}</span>
+          </h5>
+          <h5 className="detail__title">
+            Fecha de nacimiento:{" "}
+            <span className="detail__text">{character.dateOfBirth}</span>
+          </h5>
+          <h5 className="detail__title">
+            Vivo: <span className="detail__text">{character.alive}</span>
+          </h5>
+          <h5 className="detail__title">
+            Especie: <span className="detail__text">{character.species}</span>
+          </h5>
+          <h5 className="detail__title">
+            Casa: <span className="detail__text">{character.house}</span>
+          </h5>
+          <h5 className="detail__title">
+            Categoría de mago:{" "}
+            <span className="detail__text">True {character.wizard}</span>
+          </h5>
+          <Link className="button" to="/">
+            Volver
+          </Link>
+        </section>
+      </div>
+    </>
   );
 }
 
